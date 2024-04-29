5c42c755687435e6abf7d3dd11c9a4b1b2b6f15c
Juuso <TIVIDIGI-14@DESKTOP-KU0JLJB>
2024-04-29 09:12:08 +0300

    Incorporated traits. Removed speed and changed from % based to flat numbers.

--- HistasiaHahmot/Program.cs
+++ HistasiaHahmot/Program.cs
@@ -12,15 +12,17 @@ namespace QuestGame
     {
         public string Name { get; set; }
         public string GameCharacter { get; set; }
-        public string GoodTrait { get; set; }
-        public string BadTrait { get; set; }
 
         // Combat Related
         readonly int maxHealth = 100;
         int health = 100;
-        public int Defense { get; set; }
-        public int AttackBonus { get; set; }
-        public int GatheringSkill { get; set; }
+        public int _defense { get; set; }
+        public int _attackBonus { get; set; }
+        public int _gatheringSkill { get; set; }
+
+        private int _strengthModifier = 0;
+        private int _intelligenceModifier = 0;
+        private int _dodgeHitModifier = 0;
 
         // Properties
         public int Health
@@ -41,52 +43,131 @@ namespace QuestGame
                 }
             }
         }
+        public int StrengthModifier
+        {
+            get { return _strengthModifier; }
+        }
+        public int IntelligenceModifier
+        {
+            get { return _intelligenceModifier; }
+        }
+        public int DodgeHitModifier
+        {
+            get { return _dodgeHitModifier; }
+        }
+
+        public int AttackBonus
+        {
+            get { return _attackBonus + StrengthModifier; }
+        }
+        public int Defense
+        {
+            get { return _defense + _dodgeHitModifier; }
+        }
+        public int GatheringSkill
+        {
+            get { return _gatheringSkill + _intelligenceModifier;  }
+        }
 
         // Constructors
-        public Character(string name, string goodTrait, string badTrait)
+        public Character(string name)
         {
             Name = name;
-            GoodTrait = goodTrait;
-            BadTrait = badTrait;
         }
+
+
+        public void SetTrait(string trait)
+        {
+            switch (trait)
+            {
+                case "Vahva":
+                    _strengthModifier += 5; // Voimaa lisääntyy +X
+                    break;
+                case "Notkea":
+                    _dodgeHitModifier += 3; // Väistömahdollisuus lisääntyy +X
+                    break;
+                case "Viisas":
+                    _intelligenceModifier += 5; // Älykkyyttä lisääntyy +X
+                    break;
+                case "Heikko":
+                    _strengthModifier -= 5; // Voimaa vähennetään +X
+                    break;
+                case "Hölmö":
+                    _intelligenceModifier -= 5; // Älykkyyttä vähennetään +X
+                    break;
+                case "Ajattelematon":
+                    _dodgeHitModifier -= 3; // Väistömahdollisuus vähennetään +X
+                    break;
+            }
+        }
+
 
         public void PrintInfo()
         {
             Console.WriteLine("Pelaajahahmosi:");
-            Console.WriteLine($"Hahmo: {Name} \nHyvä ominaisuus: {GoodTrait} \nHuono ominaisuus: {BadTrait}");
+            Console.WriteLine($"Hahmo: {Name} \nVoima ({_strengthModifier}): {GetStrengthTrait()} \n" +
+                $"Älykkyyden ({_intelligenceModifier}): {GetIntelligenceTrait()} \nVäistö ({_dodgeHitModifier}): {GetDodgeTrait()}");
             Console.WriteLine("***********************************\n");
         }
+
+        private string GetStrengthTrait()
+        {
+            if (_strengthModifier > 0)
+                return "Vahva";
+            else if (_strengthModifier < 0)
+                return "Heikko";
+            else
+                return "Ei mitään";
+        }
+
+        private string GetIntelligenceTrait()
+        {
+            if (_intelligenceModifier > 0)
+                return "Viisas";
+            else if (_intelligenceModifier < 0)
+                return "Hölmö";
+            else
+                return "Ei mitään";
+        }
+
+        private string GetDodgeTrait()
+        {
+            if (_dodgeHitModifier > 0)
+                return "Notkea";
+            else if (_dodgeHitModifier < 0)
+                return "Ajattelematon";
+            else
+                return "Ei mitään";
+        }
+
+
     }
 
     class Program
     {
         static Random random = new Random();
-
-        static string GenerateGoodTrait()
+        static string GenerateTrait()
         {
-            string[] goodTraits = { "Vahva", "Notkea", "Viisas", "Nopea", "Ei mitään" };
-            return goodTraits[random.Next(goodTraits.Length)];
-        }
-
-        static string GenerateBadTrait()
-        {
-            string[] badTraits = { "Heikko", "Hidas", "Hölmö", "Ajattelematon", "Ei mitään" };
-            return badTraits[random.Next(badTraits.Length)];
+            string[] allTraits = { "Vahva", "Notkea", "Viisas", "Heikko", "Hölmö", "Ajattelematon" };
+            return allTraits[random.Next(allTraits.Length)];
         }
 
         static Character GenerateCharacter()
         {
-            string[] names = { "Heimopäällikkö", "Sotilas", "Kyläläinen", };
+            string[] names = { "Heimopäällikkö", "Sotilas", "Kyläläinen" };
             string name = names[random.Next(names.Length)];
 
+            var character = new Character(name);
 
-            string goodTrait = GenerateGoodTrait();
-            string badTrait = GenerateBadTrait();
+            character.SetTrait(GenerateTrait()); // Voima
+            character.SetTrait(GenerateTrait()); // Älykkyys
+            character.SetTrait(GenerateTrait()); // Väistö
 
-            return new Character(name, goodTrait, badTrait);
+            return character;
         }
 
-            static void Main(string[] args)
+
+        static void Main(string[] args)
         {
             Console.WriteLine("Hei! Generoidaan sinulle hahmo!");
             Console.WriteLine("\nAnna hahmollesi nimi: ");
@@ -98,10 +179,6 @@ namespace QuestGame
 
             // Char info printing
             player.PrintInfo();
-
-            // Temp Stats
-            player.GatheringSkill = 2;
-            player.AttackBonus = 5;
 
             TheGame.StartAdventure(player);
 
