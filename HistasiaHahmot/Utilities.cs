41ee3105e85612aef0a647f0a1fc8b9cc30b7499
Juuso <TIVIDIGI-14@DESKTOP-KU0JLJB>
2024-04-29 09:46:18 +0300

    Added Utilities Class for Text writing method and a Press to Continue Method

---
+++ HistasiaHahmot/Utilities.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QuestGame
+{
+    public class Utilities
+    {
+
+        static int s_textSpeed = 30;
+        static int s_selectionDelay = 2000;
+
+        // Method for text writing
+        public static void TextWriter(string text)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(s_textSpeed);
+            }
+        }
+
+        // Press button to continue
+        public static void PressToContinue()
+        {
+            Thread.Sleep(s_selectionDelay);
+            Console.Write("\nPaina nappia jatkaaksesi");
+            Console.ReadKey(true);
+        }
+    }
+
+    public static class Utility
+    {
+        public static int s_TextChapterDelay = 1500;
+
+    }
+}
